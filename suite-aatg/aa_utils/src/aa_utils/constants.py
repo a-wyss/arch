8dad83cccce1560d157ecdfe4c2de987eb4544a0
a-wyss <andreas.wyss@tg.ch>
2023-03-14 13:40:10 +0100

    init commit

---
+++ suite-aatg/aa_utils/src/aa_utils/constants.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class RasterBandDataType(Enum):
+    GDT_Unknown = 0
+    GDT_Byte = 1
+    GDT_UInt16 = 2
+    GDT_Int16 = 3
+    GDT_UInt32 = 4
+    GDT_Int32 = 5
+    GDT_Float32 = 6
+    GDT_Float64 = 7
+    GDT_CInt16 = 8
+    GDT_CInt32 = 9
+    GDT_CFloat32 = 10
+    GDT_CFloat64 = 11
+    GDT_TypeCount = 12
